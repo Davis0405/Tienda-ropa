a977e856434bad5cffbd62954c31d35c0e82a587
Davis0405 <160440751+Davis0405@users.noreply.github.com>
2025-03-05 00:07:58 -0600

    Mensaje descriptivo de lo que se ha hecho

--- productos/views.py
+++ productos/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Categoria
 
 def lista_productos(request):
-    productos = Producto.objects.all()
-    return render(request, 'productos/lista_productos.html', {'productos': productos})
+    # Obtener todas las categorías
+    categorias = Categoria.objects.all()
+    
+    # Obtener la categoría seleccionada (si hay alguna)
+    categoria_id = request.GET.get('categoria')
+    
+    if categoria_id:
+        productos = Producto.objects.filter(categoria_id=categoria_id)
+    else:
+        productos = Producto.objects.all()
+
+    return render(request, 'productos/lista_productos.html', {
+        'productos': productos,
+        'categorias': categorias,
+        'selected_categoria': categoria_id
+    })
+
 
 from django.shortcuts import render, redirect
 from .forms import ProductoForm
